76707e51ed03107d3b6e43612b3828e3233e68b8
water_wind_ <blwaterwind@gmail.com>
2025-10-22 20:08:26 -0400

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,16 +2,15 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
-
 import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://example.com",
+	site: "https://brandonwalkers.sanctumnetwork-1.workers.dev",
 	integrations: [mdx(), sitemap()],
 	adapter: cloudflare({
 		platformProxy: {
-			enabled: true,
+			enabled: false, // Disable to avoid injecting experimental Permissions-Policy headers
 		},
 	}),
 });
